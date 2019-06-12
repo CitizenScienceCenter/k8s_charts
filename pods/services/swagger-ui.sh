f93fd407346c34c414803541fd9850dc03c42a50
encima <encima@gmail.com>
2019-06-12 13:57:50 +0200

    added grafana service

--- pods/services/swagger-ui.sh
+++ pods/services/swagger-ui.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-export NAME=swaggerui-staging
-export NS=c3s-staging
+export NAME=swaggerui-prod
+export NS=c3s-prod
 export TAG=latest
 export IMG=swagger-ui
 export PORT=8080
-export HOST=openapi-staging.citizenscience.ch
+export HOST=openapi.citizenscience.ch
 envsubst < ../env-deploy.yaml > ${NAME}.deploy.yaml
